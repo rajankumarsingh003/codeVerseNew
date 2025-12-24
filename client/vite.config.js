68f054a6a53a71d65393967e0725d3a0fdfb67df
Rajan <cpsrajan2002@gmail.com>
2025-12-24 22:23:09 +0530

    Fix Vite build for Vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/', // React Router ke liye SPA fallback
-// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ✅ Correct Vite config for React Router + Vercel
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 Use relative paths so Vercel finds JS files correctly d
-})
+});
